3f507f0ff51bc1027304ba04d9886bfb5202d02e
Gebbia <gebbianatasha@gmail.com>
2022-08-17 18:03:52 -0500

    updated starter code & created classes

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,8 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student natasha = new Student("Natasha", 1, 1, 4.0);
+            Console.WriteLine("The Student class works! " + natasha.Name + " is a student!");
         }
     }
 }
